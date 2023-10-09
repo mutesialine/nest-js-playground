db8b427c371cfacd20872054cb8546f4d53f12fe
mutesialine <ktesialine@gmail.com>
2023-10-09 17:47:24 +0200

    get url param

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
   @Get()
   findAll(): string {
-    return 'get all items';
+    return 'get all item';
   }
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return ` Name:${createItemDto.name}, Desc:${createItemDto.description}, quantity:${createItemDto.qty}`;
